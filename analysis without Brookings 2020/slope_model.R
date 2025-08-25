b323f37c284dfce3371043968b135b8203b1caaa
baner175 <baner175@umn.edu>
2025-08-26 00:14:02 +0530

    Adding analyisis without Brookings 2020

---
+++ analysis without Brookings 2020/slope_model.R
@@ -0,0 +1,54 @@
+rm(list = ls())
+library(lme4)
+library(MASS)
+library(multcomp)
+df <- read.csv('../QP_fit_data.csv', header = TRUE)
+df <- df[!(df$SITE == 'Brookings' & df$YEAR == 2020), ]
+dim(df)
+head(df)
+df$YEAR <- as.factor(df$YEAR)
+df$SITE <- as.factor(df$SITE)
+df$COVERCROP <- as.factor(df$COVERCROP)
+str(df)
+
+
+# fixed: cover crop; random: year:site
+m0 <- lmer(b ~ COVERCROP + (1|YEAR:SITE),
+           data = df)
+summary(m0)
+car::Anova(m0, test = 'F')
+fixef(m0)
+
+pair_wise <- matrix(c(0,1,0,
+                      0,0,1,
+                      0,1,-1), nrow = 3, byrow = TRUE)
+rownames(pair_wise)<- c('NC - MC', 'SC - MC', 'NC - SC')
+
+pair_wise_glht <- glht(m0, linfct = pair_wise,
+                       adjust = 'tukey')
+summary(pair_wise_glht)
+
+diff_effect <- matrix(c(1,0,0,
+                        1,1,0,
+                        1,0,1), nrow = 3, byrow = TRUE)
+rownames(diff_effect)<- c('MC', 'NC', 'SC')
+diff_effect_glht <- glht(m0, linfct = diff_effect,
+                         adjust = 'tukey')
+summary(diff_effect_glht)
+
+confint(diff_effect_glht, level = 0.67)
+confint(diff_effect_glht, level = 0.95)
+
+
+plot(b~COVERCROP, data = df,
+     main = 'Distribution of linear term across different covercrops',
+     xlab = 'Covercrop', ylab = 'Linear term',
+     col = c('darkgreen', 'blue', 'orange'))
+
+
+# fixed: cover crop + site; random: year
+m1 <- lmer(b ~ COVERCROP + SITE + (1|YEAR),
+           data = df)
+summary(m1)
+car::Anova(m1, test = 'F')
+fixef(m1)
